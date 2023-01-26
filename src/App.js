b5b2c4284f253e2ce467d6169d8e213eb724bdfa
isachinraj4 <57107499+isachinraj4@users.noreply.github.com>
2023-01-26 21:21:07 +0530

    This is my latest commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
+import React, { useState } from 'react'
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+
 
 function App() {
+  const[mode, setMode] = useState('light');
+  const[alert, setAlert] = useState(null);
+  
+  const showAlert = (_message, _type) => {
+      setAlert({
+        message: _message,
+        type: _type
+      })
+      setTimeout(() => {
+        setAlert(null);
+      }, 1500);
+  }
+
+  const toggleMode = () => {
+    if(mode === 'light'){ 
+      setMode('dark')
+      document.body.style.backgroundColor = '#383b3d' 
+      showAlert("Dark mode has been enabled", "success")
+    }else { 
+      setMode('light')     
+      document.body.style.backgroundColor = 'white'
+      showAlert("Light mode has been enabled", "success")
+    }
+  }
   return (
     <>
-<Navbar title = "ApnaMessWallah" aboutText = "About us" feedback = "Feedback"/>
-<div className="container my-3">
-  <TextForm heading = "Please provide you feedback"/>
-
-</div>
+      <Router>
+        <Navbar title = "ApnaMessWallah" aboutText = "About us" feedback = "Feedback" mode={mode} toggleMode = {toggleMode} />
+        <Alert alert = {alert}/>
+        <div className="container my-3">
+          <Routes>
+            <Route exact path='/' element={<TextForm heading = "Please provide your feedback" mode = {mode} showAlert= {showAlert}/>} />
+            <Route exact path='/feedback' element= {<TextForm heading = "Please provide your feedback" mode = {mode} showAlert= {showAlert}/>} />
+            <Route exach path='/about'element = {<About mode = {mode}/>}/>
+          </Routes>
+        </div> 
+      </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,21 +5,47 @@ export default function TextForm(props) {
 
     const onSubmit = () => {
         console.log("feedback submitted")
+        let newFeedback = text.toUpperCase();
+        setText(newFeedback);
+        props.showAlert("Feedback submitted!", "success");
     }
 
-    const handelOnChange = () => {
-        setText(this)
-        console.log("valueis changing")
+    const onCancle = () => {
+        setText("");
+        props.showAlert("Feedback cancled!", "success");
+    }
+
+    const handelOnChange = (event) => {
+        setText(event.target.value);
+    }
+
+    const removeSpaces = () =>{
+        let newTxt = text.split(/[ ]+/);
+        setText(newTxt.join(" "));
+        props.showAlert("Extra spaces removed from feedback!", "success");
     }
 
     return (
-    <div>
-        <div className="mb-3">
-            <h1>{props.heading}</h1>
-            {/* <label htmlFor="textExp" className="form-label">Example textarea</label> */}
-            <textarea className="form-control" value={text} onChange = {handelOnChange} id="exampleFormControlTextarea1" rows="3"></textarea>
+    <>
+        <div>
+            <div className="mb-3" style={{color: props.mode === 'dark'? 'white': 'black'}}>
+                <h1>{props.heading}</h1>
+                <textarea className="form-control" style={{backgroundColor: props.mode === 'dark'? 'gray': 'white', color: props.mode === 'dark'? 'white': 'black'}}
+                 value={text} onChange = {handelOnChange} id="myBox" rows="3"></textarea>
+            </div>
+            <button className="btn btn-primary mx-2" onClick={onSubmit} disabled= {text.trim().length === 0? true: false}>Submit</button>
+            <button className="btn btn-primary mx-2 my-3" onClick={onCancle}>Cancle</button>
+            <button className="btn btn-primary mx-2" onClick={removeSpaces}>Remove Extra Spaces</button>
         </div>
-        <button className="btn btn-primary" onClick={onSubmit}>Submit</button>
-    </div>
+
+        <div className="container my-3" style={{color: props.mode === 'dark'? 'white': 'black'}}>
+            <h1>Your feedback info</h1>
+            <p>{(text.trim().length === 0) ? 0 : text.trim().split(/[ ]+/).length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} minutes takes to read</p>
+            <h2>Feedback preview</h2>
+            <p>{text.length > 0 ? text: "Enter you feedback above to preview it here!"}</p>
+
+        </div>
+    </>
     )
 }
